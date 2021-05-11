6ab60a74135207d5e4e15aa71c5054fa9bc3bcdb
burkanov-e <burkanov_e@auca.kg>
2021-05-11 23:13:07 +0600

    Problem08 Finished

--- Project-01/problem08.dart
+++ Project-01/problem08.dart
@@ -1,1 +1,15 @@
+void main(List<String> args) {
+  int fib1 = 1;
+  int fib2 = 1;
+  int n = 6;
 
+  int i = 0;
+
+  while (i < n - 2) {
+    int fibSum = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = fibSum;
+    i++;
+    print(fib2);
+  }
+}
