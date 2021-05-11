4ea7cb6d5b062a57fc99787b739b2179b9fb5101
burkanov-e <burkanov_e@auca.kg>
2021-05-11 23:12:47 +0600

    Problem08 Finished Problem11 Unsolved

---
+++ Project-01/problem11.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'dart:core';
+
+main(List<String> args) {
+  int N = int.parse(stdin.readLineSync()!);
+  var arr = new List.filled(N, null, growable: false);
+
+  for (int i = 0; i < N; i++) {
+    var input = int.parse(stdin.readLineSync()!);
+  }
+}
